475fef87a5866cf5e1f129cb86d99db1f831a76f
fenglh2869 <fenglihai@fenglihaideMacBook-Pro.local>
2017-12-19 18:38:33 +0800

    优化crawl_keyword_url 方法

--- jdspider.py
+++ jdspider.py
@@ -14,24 +14,19 @@ from pyspider.database.mysql.MySQLHandle import SQL
 NUMBER_START = 1
 NUMBER_END = 100
 
-#self.crawl(url, callback=self.list_page, itag=1, fetch_type='js', save={"category":each.attr.title, 'keyword':params['keyword'], "page":page}
 
 class Handler(BaseHandler):
 
-
+    category_urls = [
+        {'url':''},
+    ]
     crawl_config = {
         'itag': 'v163', 'connect_timeout':120,
         'headers' : {'Connection':'keep-alive','Accept-Encoding':'gzip, deflate, br','Accept-Language':'zh-CN,zh;q=0.8','content-type':'application/x-www-form-urlencoded','Referer':'http://channel.jd.com/men.html','User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
     }
 
-    @every(minutes= 60 * 24)
-    def on_start(self):
-
-        self.crawl('http://channel.jd.com/men.html', callback=self.category_page)
-
-    def get_headers(self, response):
+    def get_headers(self, referer_url):
         headers = {}
-        referer_url = response.url + '#' + str(time.time())
         headers['User-Agent'] = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE) '
         headers['Accept'] = '*/*'
         headers['Accept-Language'] = 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2'
@@ -40,19 +35,30 @@ class Handler(BaseHandler):
         headers['X-Requested-With'] = 'XMLHttpRequest'
         return headers
 
+    def crawl_keyword_url(self, keyword, base_url, callback, cookies, headers):
+        page = NUMBER_START
+        while page <= NUMBER_END:
+            s = ((page + 1) / 2 - 1) * 60 + 1
+                ##psort=3，是根据销量排序
+            page_url = base_url + '&psort=3&page=' + str(page) + '&s=' + str(s) + '&click=0'
+            page += 2
+            self.crawl(page_url,headers = headers ,cookies=cookies, callback=callback, fetch_type='js', save={"category":keyword, 'keyword':keyword, "page":page });
+
+    @every(minutes= 60 * 24)
+    def on_start(self):
+
+        self.crawl('http://channel.jd.com/men.html', callback=self.category_page)
+
+
     @config(age=60 * 60 * 24)
     def category_page(self, response):
 
         for each in response.doc('.fore3 ul a').items():
             query = urlparse.urlparse(each.attr.href).query
             params=  dict([(k,v[0]) for k,v in urlparse.parse_qs(query).items()])
-            page = NUMBER_START
-            while page <= NUMBER_END:
-                s = ((page + 1) / 2 - 1) * 60 + 1
-                url = each.attr.href + '&psort=3&page=' + str(page) + '&s=' + str(s) + '&click=0'
-                page += 2
-                headers = self.get_headers(response)
-                self.crawl(url,headers = headers ,cookies=response.cookies, callback=self.list_page, fetch_type='js', save={"category":each.attr.title, 'keyword':params['keyword'], "page":page });
+            referer_url = response.url + '#' + str(time.time())
+            headers = self.get_headers(referer_url)
+            self.crawl_keyword_url(params['keyword'],each.attr.href,self.list_page, response.cookies, headers)
 
 
     def list_page(self, response):
@@ -78,7 +84,6 @@ class Handler(BaseHandler):
                     img_src = 'http:' + data_lazy_img
                 else:
                     continue
-            #print i, img_title,img_src
             obj['url'] = img_src
             obj['title'] = img_title
             obj['category'] = category
@@ -94,7 +99,8 @@ class Handler(BaseHandler):
         show_items_string = ','.join(str(i) for i in show_items)
         url = 'http://search.jd.com/s_new.php?keyword=' + keyword + '&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&wq=' + keyword +'&page=' + str(page + 1) +'&s=26&scrolling=y&tpl=3_L&show_items=' + show_items_string
         ##headers
-        headers = self.get_headers(response)
+        referer_url = response.url + '#' + str(time.time())
+        headers = self.get_headers(referer_url)
         self.crawl(url,cookies=response.cookies,headers = headers, callback=self.page_detail, save={'results':results, 'category':category})
 
     def page_detail(self, response):
@@ -112,7 +118,6 @@ class Handler(BaseHandler):
                     img_src = 'http:' + data_lazy_img
                 else:
                     continue
-            #print i,img_src
             obj['url'] = img_src
             obj['title'] = img_title
             obj['category'] = category
@@ -129,5 +134,5 @@ class Handler(BaseHandler):
         i= 1
         for each in result:
             print i, each
-            sql.insert('table_jingdong',**each)
+            sql.insert('table_jingdong_2',**each)
             i += 1
